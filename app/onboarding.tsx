728fb9446f48903b175fadf67aa7d81ea4d02cc7
Christian Drewes <cddok20@yahoo.com>
2025-04-27 17:12:01 -0500

    init

---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const MyComponent = () => {
+  return (
+    <View>
+      <Text>My Component</Text>
+    </View>
+  );
+};
+
+export default MyComponent;
