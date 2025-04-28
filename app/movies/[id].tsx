fe82defc6328b1fe900a8cd224a6767436884426
Christian Drewes <cddok20@yahoo.com>
2025-04-28 08:55:23 -0500

    add routing home page ui elements and api calls

---
+++ app/movies/[id].tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+const MovieDetails = () => {
+
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+}
+
+export default MovieDetails;
